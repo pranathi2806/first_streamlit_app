7a73c54de5fa2dcab949e3defa4d73805ae0c8ea
pranathi2806 <144767982+pranathi2806@users.noreply.github.com>
2023-09-12 17:13:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,8 @@ streamlit.text(fruityvice_response)
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 
